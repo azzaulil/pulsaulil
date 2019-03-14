d16f65b3d494d74fc545ac9f1cddc69a1c368398
azzaulil <azzaulil50@gmail.com>
2019-03-14 07:55:08 +0700

    Updates

---
+++ precache-manifest.31b9b06aa3e5f9bc93f4abba6ba27c4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbd135191a82d282c503",
+    "url": "/pulsaulil/static/js/runtime~main.dbd13519.js"
+  },
+  {
+    "revision": "25752f1e4d6cb30012eb",
+    "url": "/pulsaulil/static/js/main.25752f1e.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/pulsaulil/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "25752f1e4d6cb30012eb",
+    "url": "/pulsaulil/static/css/main.5c2bc34f.chunk.css"
+  },
+  {
+    "revision": "7155d393fb8ff76df3b4f8f1e8940bdb",
+    "url": "/pulsaulil/index.html"
+  }
+];
