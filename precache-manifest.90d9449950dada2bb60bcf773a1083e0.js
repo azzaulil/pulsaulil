01942c6e4513d9e341d3d82d99d463b90da29b10
azzaulil <azzaulil50@gmail.com>
2019-04-29 13:13:54 +0700

    Updates

---
+++ precache-manifest.90d9449950dada2bb60bcf773a1083e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/pulsaulil/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "a53f2c7e89d40c38cb66",
+    "url": "/pulsaulil/static/js/runtime~main.2a77bfbe.js"
+  },
+  {
+    "revision": "886fe47c37f2f605285e",
+    "url": "/pulsaulil/static/js/main.dad2b871.chunk.js"
+  },
+  {
+    "revision": "bb4c9f292c6d71fc8227",
+    "url": "/pulsaulil/static/js/2.d8f9fa37.chunk.js"
+  },
+  {
+    "revision": "886fe47c37f2f605285e",
+    "url": "/pulsaulil/static/css/main.f511a0ba.chunk.css"
+  },
+  {
+    "revision": "bb4c9f292c6d71fc8227",
+    "url": "/pulsaulil/static/css/2.883d685e.chunk.css"
+  },
+  {
+    "revision": "0cfe8252559ead3af502fbae651966ec",
+    "url": "/pulsaulil/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pulsaulil/precache-manifest.0a2bdfdb2ceb7c344816f3a4a562883d.js"
+  "/pulsaulil/precache-manifest.90d9449950dada2bb60bcf773a1083e0.js"
 );
 
 workbox.clientsClaim();
